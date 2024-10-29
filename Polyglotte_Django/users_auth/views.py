cb1ce1f7c69a1b55c73835896972009728b817da
Donald Peters <donny9585@gmail.com>
2024-10-29 22:56:49 +0100

    Defined account route

--- Polyglotte_Django/users_auth/views.py
+++ Polyglotte_Django/users_auth/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UpdateForm, BioForm
+from blogapp.models import BlogPost
 
 
 # Create your views here.
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -27,6 +31,8 @@ def logout_user(request):
 
 
 def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -36,3 +42,35 @@ def register_user(request):
     else:
         form = CustomUserCreationForm()
         return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def user_account(request):
+    user = request.user
+    update_form = UpdateForm(instance=user)
+    bio_form = BioForm()
+
+    if request.method == 'POST':
+        if 'update' in request.POST:
+            update_form = UpdateForm(request.POST, instance=user)
+            if update_form.is_valid():
+                update_form.save()
+                messages.success(request, 'Your account has successfully been updated')
+                return redirect('account')
+        elif 'bio' in request.POST:
+            bio_form = BioForm(request.POST)
+            if bio_form.is_valid():
+                user.bio = bio_form.cleaned_data['bio']
+                user.save()
+                messages.success(request, 'Your bio has successfully been updated')
+                return redirect('account')
+
+    bio_form.fields['bio'].initial = user.bio
+
+    posts = BlogPost.objects.filter(user=user).order_by('-created_at')
+
+    return render(request, 'profile_page.html', {
+        'update_form': update_form,
+        'bio_form': bio_form,
+        'posts': posts
+    })
