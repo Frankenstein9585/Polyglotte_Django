cb654f5d8672256a37edb885522582b9f5288b20
Donald Peters <donny9585@gmail.com>
2024-10-29 22:41:22 +0100

    Added Update and Bio Forms

--- Polyglotte_Django/users_auth/forms.py
+++ Polyglotte_Django/users_auth/forms.py
@@ -7,3 +7,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+
+
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+
+class BioForm(forms.Form):
+    bio = forms.CharField(widget=forms.Textarea, required=False)
