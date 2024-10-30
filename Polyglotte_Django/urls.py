e4e45f7e746286dc169da4e021d56c54696a0cef
Donald Peters <donny9585@gmail.com>
2024-10-30 02:44:45 +0100

    Refactor compelete

--- Polyglotte_Django/urls.py
+++ Polyglotte_Django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Polyglotte_Django project.
+URL configuration for Polyglotte_Django_ project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
