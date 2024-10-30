2c552b864a3706badf878f6299568449394b19fe
Donald Peters <donny9585@gmail.com>
2024-10-30 02:17:08 +0100

    Cleaned settings.py file

--- Polyglotte_Django/Polyglotte_Django/settings.py
+++ Polyglotte_Django/Polyglotte_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l8j)xwkj+sztop6x4=w%yp_hvq7q#yw5t4x_=(oh(-f-$k(655"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
