90c9635751f3f31d6615485a2adbd9666b19582c
Donald Peters <donny9585@gmail.com>
2024-10-30 03:52:17 +0100

    Refactored again

--- Polyglotte_Django/wsgi.py
+++ Polyglotte_Django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Polyglotte_Django_ project.
+WSGI config for Polyglotte_Django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Polyglotte_Django_.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Polyglotte_Django.settings")
 
 application = get_wsgi_application()
