a9171fad320a495f3679bef249d22bdcd121e0b6
Donald Peters <donny9585@gmail.com>
2024-10-30 02:15:44 +0100

    Done

--- Polyglotte_Django/blogapp/urls.py
+++ Polyglotte_Django/blogapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('post/<uuid:post_id>/', views.show_post, name='show_post')
+    path('post/<int:post_id>/', views.show_post, name='show_post'),
+    path('post/', views.new_post, name='new_post'),
+    path('post/edit/<int:post_id>/', views.update_post, name='update_post'),
+    path('post/delete/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
--- Polyglotte_Django/blogapp/views.py
+++ Polyglotte_Django/blogapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .models import BlogPost, User
+from .forms import PostForm
 
 
 # Create your views here.
@@ -9,6 +13,51 @@ def index(request):
 
 
 def show_post(request, post_id):
-    post = get_object_or_404(BlogPost, id=post_id)  # This will correctly fetch the post using UUID
-    return render(request, 'show_post.html', {'post': post})
+    post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'post_page.html', {'post': post})
 
+
+@login_required
+def new_post(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            if post.category == '':
+                post.category = 'Miscellaneous'
+            post.save()
+            messages.success(request, 'Post Successfully Created')
+            return redirect('index')
+    return render(request, 'create_post.html', {
+        'title': 'New Post',
+        'form': form,
+        'legend': 'New Post'
+    })
+
+
+@login_required
+def update_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.user != request.user:
+        raise PermissionDenied
+    form = PostForm(instance=post)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have successfully updated your blogpost')
+            return redirect('show_post', post_id=post.id)
+
+    return render(request, 'create_post.html', {'form': form})
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.user != request.user:
+        raise PermissionDenied
+    post.delete()
+    messages.success(request, 'Your post has been deleted.')
+    return redirect('index')
