fbab9f4005c2be94dd321664cfd61ab14500b3b3
Donald Peters <donny9585@gmail.com>
2024-10-30 04:04:07 +0100

    Updated settings.py

--- Polyglotte_Django/settings.py
+++ Polyglotte_Django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://polyglotte-5a70da1573aa.herokuapp.com']
 
 # Application definition
 
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'blogapp/static')
 STATICFILES_DIRS = [BASE_DIR / "blogapp" / "static"]
 
 # Default primary key field type
